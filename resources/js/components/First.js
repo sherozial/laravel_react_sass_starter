a509d156adbf08c0b0e3367eeee79c744930b6cf
l.sherozia <cartonich@gmail.com>
2019-08-05 12:14:39 +0400

    ready for hacking

---
+++ resources/js/components/First.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const First = ()=>{
+    return(
+        <div className='container'>
+            <div className='row'>
+                <h1>React Component</h1>
+            </div>
+        </div>
+    )
+}
+
+export default First;
---
+++ resources/js/components/Index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import First from './First';
+
+
+ReactDOM.render(<First />, document.getElementById('root'));
